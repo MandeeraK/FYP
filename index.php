1c3e1ded7364e30ed4a8b7145ea658edeb7af0fd
MandeeraK <mandeera19@gmail.com>
2018-03-13 11:58:19 +0530

    13-03-2018

--- index.php
+++ index.php
@@ -6,59 +6,18 @@
  * Time: 10:37 PM
  */
 ?>
+<!DOCTYPE html>
 <html>
 <head>
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-
-    <!-- jQuery library -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-
-    <!-- Latest compiled JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <!--<link rel="stylesheet" href="styles.css">-->
+    <title>File Upload</title>
 </head>
 <body>
-<div class="tab">
-    <button class="tablinks" onclick="openCity(event, 'Upload2')">Upload</button>
-    <button class="tablinks" onclick="openCity(event, 'View')">All Files</button>
-</div>
-
-<div id="Upload2" class="tabcontent">
-
-    <form action="upload.php" method="post" enctype="multipart/form-data">
-        Select image to upload:
-        <input type="file" name="fileToUpload" id="fileToUpload">
-        <input type="submit" value="Upload Image" name="submit">
-    </form>
-</div>
-<div id="View" class="tabcontent">
-
-</div>
-
-<script>
-    $(function() {
-        $('input[type=file]').change(function(){
-            var t = $(this).val();
-            var labelText = 'File : ' + t.substr(12, t.length);
-            $(this).prev('label').text(labelText);
-        })
-    });
-
-    function openCity(evt, cityName) {
-        var i, tabcontent, tablinks;
-        tabcontent = document.getElementsByClassName("tabcontent");
-        for (i = 0; i < tabcontent.length; i++) {
-            tabcontent[i].style.display = "none";
-        }
-        tablinks = document.getElementsByClassName("tablinks");
-        for (i = 0; i < tablinks.length; i++) {
-            tablinks[i].className = tablinks[i].className.replace(" active", "");
-        }
-        document.getElementById(cityName).style.display = "block";
-        evt.currentTarget.className += " active";
-    }
-</script>
+<h1>Upload file here</h1>
+<form enctype="multipart/form-data" action="upload.php" method="post">
+    <input type="hidden" name="MAX_FILE_SIZE" value="100000"/>
+    Choose a file to upload: <input name="uploadedfile" type="file" /><br/>
+    <input type="submit" value="Upload File">
+</form>
 </body>
 </html>
 
--- upload.php
+++ upload.php
@@ -6,19 +6,17 @@
  * Time: 10:32 AM
  */
 
-    $target_dir = "uploads/";
-    $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-    $uploadOk = 1;
-    $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-    // Check if image file is a actual image or fake image
-    if(isset($_POST["submit"])) {
-        $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-        if($check !== false) {
-            echo "File is an image - " . $check["mime"] . ".";
-            $uploadOk = 1;
-        } else {
-            echo "File is not an image.";
-            $uploadOk = 0;
-        }
-    }
-    ?>
+// Where the file is going to be placed
+$target_path = "uploads/";
+
+/* Add the original filename to our target path.
+Result is "uploads/filename.extension" */
+$target_path = $target_path . basename( $_FILES['uploadedfile']['name']);
+
+if(move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $target_path)) {
+    echo "The file ".  basename( $_FILES['uploadedfile']['name']).
+        " has been uploaded";
+} else{
+    echo "There was an error uploading the file, please try again!";
+}
+?>
